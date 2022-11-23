e6aee4928707944a2561a504bd78fcb8d2ea0a92
CHANYUKUO <42226871+CHANYUKUO@users.noreply.github.com>
2022-11-23 13:31:26 -0800

    Rewrting Loading iamges
    Rewrte Loading>  Allow user to select loading 2d or 3d dicom images; Program provide info tab that allow user to select what to use.

--- Functions/LoadingImagesOntoMatLab.m
+++ Functions/LoadingImagesOntoMatLab.m
@@ -1,20 +1,37 @@
-function [rawImage,segmentedImage] = LoadingImagesOntoMatLab(rawImageDir,segmentedImageDir)
-    %% Load CT Images Stored as 2D Slices
+function [rawImage,segmentedImage,info] = LoadingImagesOntoMatLab(data_type)
     functionFolder = pwd;
-    cd(rawImageDir); %%(Retrieves 2D dicom image set. Selection must be of a folder, so make sure to keep dicom images within a known folder) 
-    files = dir('*.dcm'); %%(Isolates 2D dicom slices within the folder that are part of the set and have the extension .dcm) 
-    info = dicominfo(files(1,1).name); %%(Extracts releveant info of the image onto a MATLAB structure array. Only the first slice is used because the general info is the same throughout)
-    for l = 1:numel(files) %%(For loop that parses through the different slices of the dicom image)
-        img = dicomread(files(l,1).name); %%(Loads 2D image slice onto a MATLAB structure array)
-        img = info.RescaleSlope.*img + info.RescaleIntercept; %%(Converts image data to Hounsfield Units)
-        Image(:,:,l) = img; %%(Combines the different slices to create one 3D dicom image)
-    end
     
-    rawImage = double(Image); %%(Converts matrix values from symbolic values to double precision so they can undergo basic calculations)
+    switch data_type
+        case 1
+            %Load CT Images Stored as 2D Slices
+            rawImageDir = uigetdir;
+            cd(rawImageDir); %%(Retrieves 2D dicom image set. Selection must be of a folder, so make sure to keep dicom images within a known folder) 
+            files = dir('*.dcm'); %%(Isolates 2D dicom slices within the folder that are part of the set and have the extension .dcm) 
+            info = dicominfo(files(1,1).name); %%(Extracts releveant info of the image onto a MATLAB structure array. Only the first slice is used because the general info is the same throughout)
+            for l = 1:numel(files) %%(For loop that parses through the different slices of the dicom image)
+                img = dicomread(files(l,1).name); %%(Loads 2D image slice onto a MATLAB structure array)
+                img = info.RescaleSlope.*img + info.RescaleIntercept; %%(Converts image data to Hounsfield Units)
+                Image(:,:,l) = img; %%(Combines the different slices to create one 3D dicom image)
+            end
+            rawImage = double(Image); %%(Converts matrix values from symbolic values to double precision so they can undergo basic calculations)
+        case 2 
+            %Load CT Images Stored as a Volume
+            [baseName, folder] = uigetfile('*.dcm');
+            fullFileName = fullfile(folder, baseName)
+            Image = dicomread(fullFileName); %%(Loads 3D dicom image onto MATLAB)
+
+            info = dicominfo(fullFileName); %%(Extract releveant info of the image onto a MATLAB structure array with the variable name info)
+            
+           rawImage = double(Image); %%(Converts matrix values from symbolic values to double precision so they can undergo basic calculations)
+    end
+
+
 
     %% Load NIfTI File
-    cd(segmentedImageDir)
-    Image = niftiread(uigetfile("*.nii")); %%(Loads the NIfTI image onto MATLAB)
+    cd(functionFolder)
+    [baseName, folder] = uigetfile('*.nii');
+    fullFileName = fullfile(folder, baseName);
+    Image = niftiread(fullFileName); %%(Loads the NIfTI image onto MATLAB)
     vessel = zeros(size(Image,1),size(Image,2),size(Image,3));
     for k = 1:size(Image,3) %%For loop switches the x cord. and y cord. and also flips around the y axis to account for coordinate differences in MATLAB and ITK-SNAP  
         for j = 1:size(Image,2)
--- MasterScript.m
+++ MasterScript.m
@@ -4,20 +4,18 @@ clear
 addpath(pwd);
 addpath("Functions/")% Function Created by Senior Design
 addpath("Functions/Skeleton3D/"); % Function Required For centerline_func_seg
-%% Step1
-   rawImageDir = uigetdir; % load the folder containing 2D dicom CT images
-   segmentedImageFile = uigetdir; % load the folder containing segmented .nii files
 
 %% Loading Images (Step1: Modified from 2020Sean)
-[rawImage,segmentedImage] = LoadingImagesOntoMatLab(rawImageDir,segmentedImageFile); % load the specific .nii file
-
+[rawImage,segmentedVessel,info] = LoadingImagesOntoMatLab(menu('What is the Structure of the Data?','2D Volume','3D Volume')); 
+% allow user to choose either volume data. 
+%% centerline_func_seg (Step2: Modified from 2019centerline_func_seg,xyz is the center line coordinate)
+[x,y,z,skel] = centerline_func_seg(segmentedVessel);
 %% Defining TID (Step2: Modified from 2020Danielle)
-Labeled = TID(rawImage,segmentedImage);
+Labeled = TID(rawImage,segmentedVessel);
 
-%% display Labeled
-display_label(Labeled,segmentedImage,menu('What would you like to color?','1','2'))
-%% Calculating TID percentage (Step3: 2019DirectCopyFunction 2020VanessaSameCode)
-DataTable=distribution(Labeled);
+%% display Labeled (Step3: Visually Display the Segmented data and where specific label located)
+display_label(Labeled,segmentedVessel,menu('What would you like to color?','1','2'))
+%% Calculating TID percentage (Step4: 2019DirectCopyFunction 2020VanessaSameCode)
+DataTable=distribution(Labeled)
 
-%% centerline_func_seg (Step4: Modified from 2019centerline_func_seg,xyz is the center line coordinate)
-[x,y,z,skel] = centerline_func_seg(segmentedImage);
+
