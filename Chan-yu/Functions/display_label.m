a37387ae7921544984c71fecf016fafe8f27adf6
CHANYUKUO <42226871+CHANYUKUO@users.noreply.github.com>
2022-12-11 17:58:06 -0800

    Fixing TID functions and 3D Visualization
    1. TID function now only on region of interest, instead of drawing a box with buffer (updated design from 2020 )
    2. 3D Visualization of specific distribution within the region of interest. Allow user to select tissue to display in red.

--- Chan-yu/Functions/display_label.m
+++ Chan-yu/Functions/display_label.m
@@ -1,48 +1,47 @@
 function display_label(Labeled,segmentedImage,map_type)
-    %% Additional step: elongate the vessel 
-    % (only use when centerline extraction algorithm failed 
-    % at the end of vessel)
-    
-    % add additional slices at the end of the vessel to improve the result of
-    % centerline extraction
-    % 
-    elongated_slice_number=3;
-    segmentedImage=double(segmentedImage);
-    Image_2=[];
-    for i=1:(size(segmentedImage,3)+elongated_slice_number)
-        if i<1+elongated_slice_number
-            Image_2(:,:,i)=segmentedImage(:,:,1);
-        else
-            Image_2(:,:,i)=segmentedImage(:,:,i-elongated_slice_number);
-        end
-    end
-    segmentedImage=Image_2;
-    clear Image_2    
-
 %% Segmented Image Section
-    Image_binary=segmentedImage==1; 
-    %[vx,vy,vz] = ind2sub(size(Image_binary),find(Image_binary(:,:,:)));
+    figure
     col=[.7 .7 .8];
-    hiso = patch(isosurface(Image_binary,0),...
+    hiso = patch(isosurface(segmentedImage,0),...
         'FaceColor',col,'EdgeColor','none');
     axis equal;axis off;
     lighting phong;
-    isonormals(Image_binary,hiso);
-    alpha(0.5);
     set(gca,'DataAspectRatio',[1 1 1])
     camlight;
+    alpha(0.1)
     hold on;
-    
 %% Plot labeled
-
+    [x y z]=size(segmentedImage);
+    newImage = zeros(x, y, z);
     switch map_type
         case 1
-            newImage= Labeled==1;
-            [xTemp,yTemp,zTemp]=ind2sub(size(newImage),find(newImage(:,:,:)));
-            plot3(...
-                xTemp,yTemp,zTemp,...
-                'square','Markersize',4,'MarkerFaceColor','k','Color','k');
+            tissue_index =1;
+            title('fat');
         case 2
-            
+            tissue_index = 2;
+            title('muscle');
+        case 3
+            tissue_index =3;
+            title('adventitia');
+        case 4
+            tissue_index =4;
+            title('aorta');
+        case 5
+            tissue_index =5;
+            title('calcium');
     end
+    %% step 
+    for i=1:x
+        for j=1:y
+            for k=1:z
+                if Labeled(i,j,k)==tissue_index
+                    newImage(i,j,k)=1;
+                end
+            end
+        end
+    end
+    hiso = patch(isosurface(newImage,0),...
+        'FaceColor',[1,0,0],'EdgeColor','none');
+    alpha(0.5)
+
 end
--- Chan-yu/MasterScript.m
+++ Chan-yu/MasterScript.m
@@ -10,16 +10,20 @@ addpath("Functions/Skeleton3D/"); % Function Required For centerline_func_seg
 % allow user to choose either volume data. 
 %% centerline_func_seg (Step2: Modified from 2019centerline_func_seg,xyz is the center line coordinate)
 [x,y,z,skel] = centerline_func_seg(segmentedVessel);
-%% Defining TID (Step2: Modified from 2020Danielle)
+
+%% (Step3: Expansion from Vessel of interest to Region of interes)
+%% Defining TID (Step4: Modified from 2020Danielle)
 Labeled = TID(rawImage,segmentedVessel);
 
 %% display Labeled (Step3: Visually Display the Segmented data and where specific label located)
-display_label(Labeled,segmentedVessel,menu('What would you like to color?','1','2'))
-%% Calculating TID percentage (Step4: 2019DirectCopyFunction 2020VanessaSameCode)
+display_label(Labeled,segmentedVessel,menu('What would you like to color?'...
+    ,'fat','muscle','adventitia','aorta','calcium'))
+
+%% Calculating TID percentage (Step4: 2019DirectCopy Function 2020VanessaSameCode)
 DataTable=distribution(Labeled)
 
 %Transform Image(Step5)
-segmentedVessel_transformation=TransformImage(segmentedVessel,x,y,z,32,50,50);
-rawImage_transformation=TransformImage(rawImage,x,y,z,32,50,50);
+%segmentedVessel_transformation=TransformImage(segmentedVessel,x,y,z,32,50,50);
+%rawImage_transformation=TransformImage(rawImage,x,y,z,32,50,50);
 
 
