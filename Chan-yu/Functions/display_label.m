a4240681515044494390309f45e63abf0c36f9f4
CHANYUKUO <42226871+CHANYUKUO@users.noreply.github.com>
2023-01-18 21:40:58 -0800

    Volume calculation and display
    Display volume in 3d Plot

--- Chan-yu/Functions/display_label.m
+++ Chan-yu/Functions/display_label.m
@@ -1,5 +1,7 @@
 function display_label(Labeled,segmentedImage)
     userinput = true;
+    %pixel_length_ratio=2.0480; %% pixel per mm
+    unit = 'mm'
     figure(3)
     while userinput
         map_type = menu('What would you like to color?','fat','muscle','vessel wall','blood','calcium','quit');
@@ -25,6 +27,13 @@ function display_label(Labeled,segmentedImage)
                 disp('Thank you! See you soon.');
                 break
         end
+        prompt = {'Enter pixel resolution (pixel/mm)','Slice Thickness (mm)'};
+        dlgtitle = 'Voxel Resolution';
+        dims = [1 35];
+        definput = {'2.0480','0.625'};
+        answer = inputdlg(prompt,dlgtitle,dims,definput);
+        pixel_length_ratio = str2double(answer{1});
+        Slice_height_per_pixel = str2double(answer{2});
  %% Segmented Image Section
     figure(3);
     col=[.7 .7 .8];
@@ -40,11 +49,13 @@ function display_label(Labeled,segmentedImage)
     [x y z]=size(segmentedImage);
     newImage = zeros(x, y, z);
         %% step 
+        pixel_count=0;
         for i=1:x
             for j=1:y
                 for k=1:z
                     if Labeled(i,j,k)==tissue_index
                         newImage(i,j,k)=1;
+                        pixel_count=pixel_count+1;
                     end
                 end
             end
@@ -52,6 +63,9 @@ function display_label(Labeled,segmentedImage)
         hiso = patch(isosurface(newImage,0),...
             'FaceColor',[1,0,0],'EdgeColor','none');
         alpha(0.5)
+        dim = [.65 .32 .5 .5];
+        volume = pixel_count/(pixel_length_ratio)^2*Slice_height_per_pixel;
+        annotation('textbox',dim,'String',[legend_text ' size is ' num2str(volume) ' ' unit '^3'],'FitBoxToText','on');
         legend('Region Of Interest',legend_text)
     end
 end
